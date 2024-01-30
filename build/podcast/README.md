5436be852fb93149ae51dccc90856e3b1428591d
Matt Leon <108271225+wydengyre@users.noreply.github.com>
2024-01-30 12:10:56 +0700

    emit rss xml without library

--- build/podcast/README.md
+++
@@ -1,3 +0,0 @@
-Podcast dependency gets inserted here so that we don't need any Node libraries to run the site.
-
-Currently, the podcast lib we use depends on Node for a few things. We can just compile a version with stubs and we should be good to go.
--- rai/feed.ts
+++ rai/feed.ts
@@ -1,6 +1,5 @@
 import { PromisePool } from "@supercharge/promise-pool";
 import { z } from "zod";
-import { Podcast } from "../build/podcast/index.js";
 import { FetchWithErr } from "./fetch.js";
 import * as media from "./media.js";
 
@@ -76,18 +75,7 @@ class RssConvertor {
 
 		// TODO: categories
 
-		const imageUrl = new URL(
-			feed.podcast_info.image,
-			this.#raiBaseUrl,
-		).toString();
-
-		const options = {
-			// feedUrl: TODO
-			// siteUrl: TODO
-			imageUrl,
-			title: feed.title,
-			description: feed.podcast_info.description,
-		};
+		const image = new URL(feed.podcast_info.image, this.#raiBaseUrl);
 
 		const { results } = await PromisePool.for(feed.block.cards)
 			.withConcurrency(this.#poolSize)
@@ -99,20 +87,91 @@ class RssConvertor {
 			})
 			.process(this.convertCard.bind(this));
 
-		return new Podcast(options, results).buildXml();
+		const podcast: Podcast = {
+			title: feed.title,
+			description: feed.podcast_info.description,
+			// TODO: maybe this podcasts have this info?
+			language: "it",
+			image,
+			items: results,
+		};
+
+		return podcastRss(podcast);
 	}
 
-	async convertCard(card: Card) {
-		const imageUrl = new URL(card.image, this.#raiBaseUrl).toString();
-		const date = new Date(card.track_info.date);
+	async convertCard(card: Card): Promise<PodcastItem> {
+		const image = new URL(card.image, this.#raiBaseUrl);
+		const pubDate = new Date(card.track_info.date);
 		const mediaInfo = await this.#fetchInfo(card.downloadable_audio.url);
-		const url = mediaInfo.url.toString();
 		return {
 			title: card.episode_title,
 			description: card.description,
-			date,
-			imageUrl,
-			enclosure: { url, size: mediaInfo.size, type: "audio/mpeg" },
+			guid: card.episode_title, // TODO: revisit this
+			pubDate,
+			url: mediaInfo.url,
+			length: mediaInfo.size,
+			image,
 		};
 	}
+}
+
+// what about category?
+type Podcast = {
+	title: string;
+	description: string;
+	language: string; // should it just be italian? Type this better?
+	image: URL;
+	items: PodcastItem[];
+};
+
+type PodcastItem = {
+	title: string;
+	description: string;
+	guid: string; // figure this out (?)
+	pubDate: Date;
+	url: URL;
+	length: number;
+	image: URL;
+};
+
+function podcastRss(p: Podcast): string {
+	const lastBuildDate = new Date().toUTCString();
+
+	const items: string[] = p.items.map((item) => itemRss(item));
+
+	const channel = `<channel>
+	<title>${cdata(p.title)}</title>
+	<description>${cdata(p.description)}</description>
+	<lastBuildDate>${lastBuildDate}</lastBuildDate>
+	<itunes:summary>${cdata(p.description)}</itunes:summary>
+	<itunes:explicit>false</itunes:explicit>
+	<itunes:image href="${p.image.toString()}"/>
+	${items.join("")}
+	</channel>`;
+	const collapsedChannel = collapseWhitespace(channel);
+	return `<?xml version="1.0" encoding="UTF-8"?><rss xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" xmlns:psc="http://podlove.org/simple-chapters" xmlns:podcast="https://podcastindex.org/namespace/1.0" version="2.0">${collapsedChannel}</rss>`;
+}
+
+function itemRss(pi: PodcastItem): string {
+	const xml = `<item>
+			<title>${cdata(pi.title)}</title>
+			<description>${cdata(pi.description)}</description>
+			<guid isPermaLink="false">${pi.guid}</guid>
+			<pubDate>${pi.pubDate.toUTCString()}</pubDate>
+			<enclosure url="${pi.url}" length="${pi.length}" type="audio/mpeg"/>
+			<itunes:summary>${cdata(pi.description)}</itunes:summary>
+			<itunes:explicit>false</itunes:explicit>
+			<itunes:image href="${pi.image}"/>
+	</item>`;
+	return collapseWhitespace(xml);
+}
+
+const xmlNlRe = /\n\s+/g;
+function collapseWhitespace(xml: string): string {
+	return xml.replaceAll(xmlNlRe, "");
+}
+
+// TODO: we should only use this if it's necessary, calling it "escape" or something like that
+function cdata(text: string) {
+	return `<![CDATA[${text}]]>`;
 }
