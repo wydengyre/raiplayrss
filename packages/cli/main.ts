8cd50dedabef48bf61ece730b6867574c1113c93
Matt Leon <108271225+wydengyre@users.noreply.github.com>
2024-02-28 17:41:31 +0800

    cli tool (#103)

---
+++ packages/cli/main.ts
@@ -0,0 +1,38 @@
+import { writeFile } from "node:fs/promises";
+import { getPodcastFromFeed } from "@podverse/podcast-feed-parser";
+import { RssConvertConf, feedToRss } from "@raiplayrss/rai/feed.js";
+
+const feedArg = process.argv.at(-1);
+if (feedArg === undefined) {
+	console.error("missing feed URL");
+	process.exit(1);
+}
+const feedUrl = new URL(feedArg);
+console.log("loading feed at URL", feedUrl.href);
+
+const feedConf: RssConvertConf = {
+	raiBaseUrl: feedUrl,
+	poolSize: 1,
+	fetch,
+};
+const feedRss = await feedToRss(feedConf, "");
+const podcast = getPodcastFromFeed(feedRss) as {
+	episodes: { title: string; enclosure: { url: string } }[];
+};
+const episodes = podcast.episodes.map(
+	(e) => [e.title, new URL(e.enclosure.url)] as const,
+);
+
+console.log("got feed, downloading episodes");
+for (const [title, url] of episodes) {
+	const destFileName = `${title.trim()}.${url.pathname.split(".").at(-1)}`;
+	console.log(`downloading ${url} to ${destFileName}`);
+	const res = await fetch(url);
+	if (!res.ok) {
+		console.error(`failed to download ${url}`);
+		continue;
+	}
+	const file = await res.arrayBuffer();
+	await writeFile(destFileName, new DataView(file));
+}
+console.log("done");
